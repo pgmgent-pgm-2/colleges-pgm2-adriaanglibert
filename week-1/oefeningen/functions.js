425f508ade75b05503348b1fec3f3b67806d56d4
adriaan-glibert-development <info@adriaanglibert.be>
2024-11-18 15:17:26 +0100

    👌 Graded exercises functions

--- week-1/oefeningen/functions.js
+++ week-1/oefeningen/functions.js
@@ -8,3 +8,27 @@ function greetings(message = 'daar') {
 
 console.log(greetings("Anna"));
 console.log(greetings());
+
+function controlAge(age, name = 'De gebruiker') {
+    if (age >= 18) {
+        return `${name} is volwassen.`;
+    }
+
+    return `${name} is nog niet volwassen.`;
+}
+
+const isJefAdult = controlAge(20, 'Jef');
+const isErosAdult = controlAge(24);
+console.log(isJefAdult);
+console.log(isErosAdult);
+
+function mainLogic(callback) {
+    // Andere logica en meer complexe zaken.
+    return callback();
+}
+
+function myCallback() {
+    console.log('Dit is de callback functie');
+}
+
+mainLogic(myCallback);
