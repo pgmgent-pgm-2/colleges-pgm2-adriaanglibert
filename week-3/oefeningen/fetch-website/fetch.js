eba8ecb6d8c931d3a3f9475ee4e9e98587128d99
adriaan-glibert-development <info@adriaanglibert.be>
2024-12-02 13:37:46 +0100

    ✨ Fetch API

---
+++ week-3/oefeningen/fetch-website/fetch.js
@@ -0,0 +1,24 @@
+const countriesPromise = fetch('https://restcountries.com/v3.2/independent?status=true&fields=languages,capitals');
+
+// console.log(countriesPromise);
+// setTimeout(() => {
+//     console.log(countriesPromise);
+// }, 5000);
+
+countriesPromise
+    .then((response) => {
+        // return response.text();
+        console.log(response);
+        if (response.status !== 200) {
+            throw 'Er ging iets mis.';
+        }
+
+        return response.json();
+    })
+    .then((data) => {
+        // console.log(JSON.parse(data));
+        console.log(data);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
