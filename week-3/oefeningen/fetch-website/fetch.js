e521eb94ac331c7b9e6c413679c2c82e1fb06af4
adriaan-glibert-development <info@adriaanglibert.be>
2024-12-02 15:12:45 +0100

    ✨ Oefeningen fetch

--- week-3/oefeningen/fetch-website/fetch.js
+++ week-3/oefeningen/fetch-website/fetch.js
@@ -5,20 +5,45 @@ const countriesPromise = fetch('https://restcountries.com/v3.2/independent?statu
 //     console.log(countriesPromise);
 // }, 5000);
 
-countriesPromise
-    .then((response) => {
-        // return response.text();
-        console.log(response);
-        if (response.status !== 200) {
-            throw 'Er ging iets mis!';
-        }
+// countriesPromise
+//     .then((response) => {
+//         // return response.text();
+//         console.log(response);
+//         if (response.status !== 200) {
+//             throw 'Er ging iets mis!';
+//         }
 
-        return response.json();
-    })
-    .then((data) => {
-        // console.log(JSON.parse(data));
-        console.log(data);
-    })
-    .catch((err) => {
-        console.error(err);
-    });
+//         return response.json();
+//     })
+//     .then((data) => {
+//         // console.log(JSON.parse(data));
+//         console.log(data);
+//     })
+//     .catch((err) => {
+//         console.error(err);
+//     });
+
+
+async function getData(callback = null) {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/independent?status=true&fields=languages,capitals', {
+            method: 'POST',
+            body: 'Nieuw lang'
+        });
+        const data = await response.json();
+        if (callback) {
+            callback(data);
+        }
+        return data;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function showData(message) {
+    console.log('Data', message);
+}
+
+console.log('Data', getData());
+getData(showData);
