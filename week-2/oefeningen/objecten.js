ba831f5fde978e866edd130fe90401097a4d8894
adriaan-glibert-development <info@adriaanglibert.be>
2024-12-02 15:13:04 +0100

    Oefeningen Objecten

--- week-2/oefeningen/objecten.js
+++ week-2/oefeningen/objecten.js
@@ -33,3 +33,23 @@ console.log(person.address.number)
 console.log(person['address']['number']);
 
 person.phone = '+32 471 78 89 90';
+
+// Oefening 4
+
+const shoppingCart = {
+    item1: { name: "Product 1", price: 20 },
+    item2: { name: "Product 2", price: 30 },
+    item3: { name: "Product 3", price: 15 },
+};
+
+// Gebruik Object.keys() om de namen van de items weer te geven
+const itemNames = Object.keys(shoppingCart).map(key => shoppingCart[key].name);
+console.log("Namen van items:", itemNames);
+
+// Gebruik Object.values() om de prijzen van de items weer te geven
+const itemPrices = Object.values(shoppingCart).map(obj => obj.price);
+console.log("Prijzen van items:", itemPrices);
+
+// Gebruik Object.entries() om zowel de namen als prijzen van de items weer te geven
+const itemEntries = Object.entries(shoppingCart).map(item => `${item[1].name}: ${item[1].price}`)
+console.log("Namen en prijzen van items:", itemEntries);
