099b7d71dba2819971071ac8f3af11623703da3f
adriaan-glibert-development <info@adriaanglibert.be>
2024-12-17 16:28:31 +0100

    ✨ Front-end blog

---
+++ week-5/front-end-website/create.js
@@ -0,0 +1,27 @@
+import { API_URL, getPosts } from "./main.js";
+
+export function initForm() {
+    const $form = document.getElementById('create-form');
+    const $titleField = $form.querySelector('input');
+    const $descriptionField = $form.querySelector('textarea');
+
+    $form.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        await createPost({
+            title: $titleField.value,
+            description: $descriptionField.value
+        });
+
+        getPosts();
+    });
+}
+
+async function createPost(content) {
+    await fetch(`${API_URL}/posts`, {
+        method: 'POST',
+        body: JSON.stringify(content),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+}
--- week-5/front-end-website/main.js
+++ week-5/front-end-website/main.js
@@ -1,18 +1,41 @@
-async function getPosts() {
-    const response = await fetch("http://localhost:3000/posts");
+import { initForm } from "./create.js";
+
+export const API_URL = "http://localhost:3000";
+
+export async function getPosts() {
+    const response = await fetch(`${API_URL}/posts`);
     const data = await response.json();
     showPosts(data);
+}
+
+async function deletePost(id) {
+    await fetch(`${API_URL}/posts/${id}`, {
+        method: 'DELETE'
+    });
+
+    getPosts();
+}
+
+function addListenerToButtons($buttons) {
+    $buttons.forEach($button => {
+        $button.addEventListener('click', () => {
+            deletePost($button.getAttribute('data-uuid'));
+        });
+    });
 }
 
 function showPosts(posts) {
     const $list = document.getElementById('news');
 
-    $list.innerHTML = posts.map(post => `<li>
-            <article>
+    $list.innerHTML = posts.map(post => `<article>
                 <strong>${post.title}</strong>
                 <p>${post.description}</p>
-            </article>
-        </li>`).join('');
+                <button data-uuid='${post.id}'>Delete</button>
+            </article>`).join('');
+
+    const $buttons = document.querySelectorAll('button[data-uuid]');
+    addListenerToButtons($buttons);
 }
 
 getPosts();
+initForm();
